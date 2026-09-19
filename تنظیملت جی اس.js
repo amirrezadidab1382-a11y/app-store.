63c81cf4411229d66fbd23a4b71c00b8f58b99b1
amirrezadidab1382-a11y <amirrezadidab1382@gmail.com>
2026-09-19 12:13:16 +0330

    login.html added and some changes

---
+++ تنظیملت جی اس.js
@@ -0,0 +1,62 @@
+// const container = document.getElementById("container");
+// const registerBtn = document.getElementById("register");
+// const loginBtn = document.getElementById("Login")
+
+// registerBtn.addEventListener("click", () => {
+//     container.classList.add("active");
+// });
+
+// loginBtn.addEventListener("click", () => {
+//     loginBtn.classList.remove("active")
+// });
+// const logoutButton = document.getElementById("log-out")
+
+// logoutButton.addEventListener("click", function () {
+//     localStorage.removeItem("username");
+//     localStorage.removeItem("password");
+
+//     window.location.href = "login.html"
+// })
+
+
+
+// const signupForm = document.getElementById("signupForm");
+// const profile = document.getElementById("profile");
+
+// const savedUsername = localStorage.getItem("username");
+// const savedPassword = localStorage.getItem("password")
+
+
+// if (savedUsername) {
+
+//     signupForm.style.display = "none";
+
+//     profile.style.display = "block";
+
+//     document.getElementById("profileUsername").textContent = savedUsername;
+//     document.getElementById("profilepassword").textContent = savedPassword
+
+// } else {
+
+//     signupForm.style.display = "block";
+
+//     profile.style.display = "none";
+
+// }
+
+
+// signupForm.addEventListener("submit", function(event) {
+
+//     event.preventDefault();
+
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
+
+
+//     localStorage.setItem("username", username);
+//     localStorage.setItem("password", password);
+
+
+//     location.reload();
+
+// });
